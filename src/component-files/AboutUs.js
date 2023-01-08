3a0e9364fcd82a08e83fe4673de42a8da3056aa5
hariharan.a <harih@LAPTOP-IQEGLE0C>
2023-01-08 21:57:15 +0530

    Corrections Aboutus

--- src/component-files/AboutUs.js
+++ src/component-files/AboutUs.js
@@ -4,17 +4,19 @@ import "../styles/AboutUs.css";
 const AboutUs = () => {
   return (
     <div className="learnaboutus">
-      <h3 className="font-monospace text-warning abouthead">Learn About Us</h3>
+      <h2 className=" abouthead">Learn About Us</h2>
       <div className="tag">
         <p className=" lh-lg paragraph">
-          &emsp;Minim sint exercitation qui reprehenderit consequat qui aliqua
-          non ipsum quis quis cupidatat velit. Ad tempor tempor ad nisi ea
-          laborum. Duis in nostrud anim magna tempor ex. Duis reprehenderit id
-          excepteur esse occaecat mollit laboris. Labore cillum mollit duis
-          veniam aliqua aliqua irure proident cupidatat incididunt aute deserunt
-          tempor. Fugiat excepteur officia voluptate reprehenderit sunt
-          consectetur.Minim sint exercitation qui reprehenderit consequat qui
-          aliqua non ipsum quis quis cupidatat velit.
+          The Atlas Law Consultancy was established to raise voice for the poor
+          and to fight for the justice that deserve. We believe everyone has a
+          right to fight for justice and to be represented by a law firm that
+          works and cares about you under different circumstances. Everyone is
+          different, including their situation, and deserves a chance to be
+          heard by a lawyer who cares and fight for you.
+          <br />
+          If you were affected by a false allegations, domestic violence, or for
+          any legal advice, contact Atlas Law Consultancy today. We are here to
+          help you and will fight to get you the justice you deserve.
         </p>
       </div>
     </div>
